236c7db2156b8758035e670a77dc184f9abaacc5
Acer <eaindraymay61@gmail.com>
2019-07-21 09:59:53 +0630

    first commit

---
+++ src/main/java/com/example/springmvc/SpringmvcApplication.java
@@ -0,0 +1,37 @@
+package com.example.springmvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+    private static Logger logger= LoggerFactory.getLogger(SpringmvcApplication.class);
+    @Autowired
+    private ApplicationContext applicationContext;
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcApplication.class, args);
+    }
+    @Bean @Profile("dev")
+    public CommandLineRunner run(){
+        return args -> {
+            logger.error("Sample Logger Message");
+            logger.warn("Sample Logger Message");
+            logger.info("Sample Logger Message");
+            logger.trace("Sample Logger Message");
+            logger.trace("Sample Logger Message");
+            logger.debug("Sample Logger Message");
+            Arrays.stream(this.applicationContext.getBeanDefinitionNames()).sorted().forEach(System.out::println);
+        };
+    }
+
+}
